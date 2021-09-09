5ee00e6af5d8de5e1c12e2bae0b807b3e2e561d6
Maxswell Sousa Diniz <maxswellsousadiniz@yahoo.com>
2021-09-09 19:40:24 -0300

    Update sentenca_1.js

--- 2- Iniciando com programação/2 ª Sentença de código/senteca_1/sentenca_1.js
+++ 2- Iniciando com programação/2 ª Sentença de código/senteca_1/sentenca_1.js
@@ -5,6 +5,4 @@
 console.log("No JavaScript todo fim de sntença não é obrigatório terminar com (;) mas muitos usam essa prática")
 
 // Aqui um exemplo de que pode ter mais de uma sentença na mesma linha dando o mesmo resultado de execução.
-console.log("Testando Primeira Sentença");
-Console.log("Testando Segunda sentença");
-console.log("testando terceira sentença");
+console.log("Testando Primeira Sentença"); Console.log("Testando Segunda sentença"); console.log("testando terceira sentença");
