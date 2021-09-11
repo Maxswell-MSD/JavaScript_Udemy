82672cb01aeae7792f3ce9d07b6c196321b4a91d
Maxswell Diniz <maxswellsousadiniz@yahoo.com>
2021-09-11 11:03:15 -0300

    Atualizando Bloco de códigos e desafios de sentença e bloco

---
+++ 2- Iniciando com programação/4 ª Bloco de Códigos/bloco.js
@@ -0,0 +1,34 @@
+/*
+*   Agrupamento de código de sentença em Bloco de chaves
+*  Comando para executar o script no terminal vscode (CTRL + ALT + N)
+*  Nesse exemplo temos dois blocos com 5 sentença de códigos, ao colocar o cursor do mouse em uma das chaves
+*  vai mostrar o seu fechamento, e ao clicar na seta das chaves o bloco vai ocultar as sentenças de códigos.
+
+ */
+
+
+{
+    console.log("Entendendo o Bloco de chaves para guardar as Sentenças de código")
+    console.log("Lembrando que no JavaScript não é necessário terminar com (;)")
+    console.log("Testando a ferramenta de bloco de chaves de abertura e fechamento")
+}
+
+{
+    console.log("Colocando mais uma sentença")
+    console.log("Colocando a última sentença de código")
+}
+
+// Criação de blocos de abertura e fechamento de chaves para entendimento , observando que cada abertura
+// vai corresponder a uma linha diferente e o mesmo se aplica ao fechamento de blocos. 
+
+{
+    {
+        {
+            {
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ 2- Iniciando com programação/5 ª Desafio Sentenças e Bloco/imprimindo_sentença_bloco.js
@@ -0,0 +1,27 @@
+/*
+Imprimindo na tela as sentenças ímpares e pares na tela dentro de bloco diferente cada um em sua linha
+específica.
+
+*/
+
+{
+    console.log("Valores ímpares")
+    console.log("1")
+    console.log("3")
+    console.log("5")
+    console.log("7")
+    console.log("9")
+    console.log("11")
+    
+}
+
+
+{
+    console.log("Valores pares")
+    console.log("2")
+    console.log("4")
+    console.log("6")
+    console.log("8")
+    console.log("10")
+    console.log("12")
+}
